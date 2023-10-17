f58d65a1b2e22f37147b9ebcb54e5437070bcf74
Antônio Ryan Araújo Ramos <antonioryanaraujo@gmail.com>
2023-10-17 11:26:09 -0300

    Edição de Veículos

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import cadastrar_carro, listar_carro
+from .views import cadastrar_carro, listar_carro, editar_carro
 
 
 urlpatterns = [
     path('cadastrar_carro/', cadastrar_carro, name='cadastrar_carro'),
     path('listar_carro/', listar_carro, name='carro_list'),
+    path('editar_carro/<int:pk>', editar_carro, name='editar_carro'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .forms import CarroForm
 from .models import Carro
 
@@ -15,3 +15,15 @@ def cadastrar_carro(request):
 def listar_carro(request):
     carros = Carro.objects.all()
     return render(request, 'carro_list.html', {'lista': carros})
+
+
+def editar_carro(request, pk):
+    carro = get_object_or_404(Carro, pk=pk)             
+    if request.method == 'POST':                        
+        form = CarroForm(request.POST, instance=carro)  
+        if form.is_valid():
+            form.save()
+            return redirect('carro_list')
+    else:
+        form = CarroForm(instance=carro)
+    return render(request, 'carro_form.html', {'form': form})
